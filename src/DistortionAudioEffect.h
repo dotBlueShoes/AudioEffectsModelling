10f01c13a7763c1a3e253fefdeb25df24fd24680
Karol D <karold@tutanota.com>
2023-12-12 22:51:53 +0100

    dry wet from distortion deleted

--- src/DistortionAudioEffect.h
+++ src/DistortionAudioEffect.h
@@ -7,8 +7,6 @@ class DistortionAudioEffect : public AudioEffect
 private:
 	int type = 0;
 	float gain = 0;
-	int dry = 0;
-	int wet = 0;
 public:
 	void applyEffect() override {
 		// Implementation specific to DelayAudioEffect
