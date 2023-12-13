bd8c0c59ef5d07f348bfee6be810b1fd379d3897
Karol D <karold@tutanota.com>
2023-12-13 03:39:03 +0100

    distortions i guess working (3 types) but needs some fine tuning

--- src/DistortionAudioEffect.h
+++ src/DistortionAudioEffect.h
@@ -8,8 +8,11 @@ private:
 	int type = 0;
 	float gain = 1;
 	float kParameter = 2; 
-	float drySound = 10;
-	float wetSound = 0;
+	//float drySound = 10;
+	//float wetSound = 0;
+
+	size cachedDrySoundSize = 0;
+	size cachedWetSoundSize = 0;
 public:
 
 	void getWetSoundSize(const size& drySoundSize, size& wetSoundSize) override;
