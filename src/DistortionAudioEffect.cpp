42638791de2405e37c44555c61b9f5284aaf8175
Karol D <karold@tutanota.com>
2023-12-13 12:18:43 +0100

    chyba naprawione

--- src/DistortionAudioEffect.cpp
+++ src/DistortionAudioEffect.cpp
@@ -16,10 +16,13 @@ void DistortionAudioEffect::applyEffect(const int16_t* drySoundData, SoundIO::Re
     float maxVal = 0.0f;
     float minVal = 0.0f;
 
+    // The maximum value for scaling from int16_t to float range [-1.0, 1.0]
+    const float maxInt16 = static_cast<float>(std::numeric_limits<int16_t>::max());
+
     // First pass: Apply distortion and find max/min values for normalization
     for (size i = 0; i < cachedDrySoundSize; ++i) {
-        // Convert to float for processing
-        float sample = sound.pcmData[i];
+        // Convert to float and scale to range [-1.0, 1.0]
+        float sample = sound.pcmData[i] / maxInt16;
 
         if (type == 0) {
             // Simple clipping distortion
@@ -27,35 +30,40 @@ void DistortionAudioEffect::applyEffect(const int16_t* drySoundData, SoundIO::Re
             sample = std::max(-1.0f, std::min(sample, 1.0f)); // Clipping to [-1, 1]
         }
         else if (type == 1) {
-            // weird distortion
-            sample = gain* ((sample * sample) / 3.0f);//((sample * 3.0f) / 2.0f);// *(1.0f - ((sample * sample) / 3.0f)); // Apply your distortion formula
+            // Weird distortion
+            sample = gain * ((sample * 3.0f) / 2.0f) * (1.0f - ((sample * sample) / 3.0f));
         }
         else if (type == 2) {
-            // weirder distortion
+            // Weirder distortion
             sample = gain * (tanh(sample * kParameter));
         }
 
-        // Store the processed sample back
-        sound.pcmData[i] = sample;
+        // Store the processed sample back, scaling it to the original int16_t range
+        sound.pcmData[i] = static_cast<int16_t>(std::max(-maxInt16, std::min(sample * maxInt16, maxInt16)));
 
         // Update max/min values for normalization
         maxVal = std::max(maxVal, sample);
         minVal = std::min(minVal, sample);
     }
+    spdlog::info("maxVal : {}", maxVal);
+    spdlog::info("minVal : {}", minVal);
 
     // Compute normalization factor
     float normalizationFactor = std::max(std::abs(maxVal), std::abs(minVal));
+    spdlog::info("normalizationFactor : {}", normalizationFactor);
 
     // Second pass: Normalize and scale back to int16_t
     if (normalizationFactor > 0.0f) {
         for (size i = 0; i < cachedDrySoundSize; ++i) {
-            float sample = sound.pcmData[i] / normalizationFactor;
-            // Scale back to int16_t range
-            sound.pcmData[i] = static_cast<int16_t>(sample * 32767.0f);
+            float sample = sound.pcmData[i] / maxInt16; // Scale to [-1.0, 1.0] range
+            sample = sample / normalizationFactor; // Normalize
+            sound.pcmData[i] = static_cast<int16_t>(sample * maxInt16); // Scale back to int16_t range
         }
     }
 
-
+    spdlog::info("maxVal : {}", maxVal);
+    spdlog::info("minVal : {}", minVal);
+    spdlog::info("normalizationFactor : {}", normalizationFactor);
 
 
    /* if (type == 0)
@@ -98,7 +106,7 @@ void DistortionAudioEffect::DisplayEffectWindow()
     ImGui::Begin(windowTitle); 
 
     ImGui::SliderInt("Type [0, 1]", &type, 0, 2);
-    ImGui::SliderFloat("Gain [-]", &gain, 0, 5);
+    ImGui::SliderFloat("Gain [-]", &gain, 0, 10);
     ImGui::SliderFloat("Parameter k [-]", &kParameter, 0, 2);
 
 
